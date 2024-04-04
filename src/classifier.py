ab09c9781852525ee57eae22f7a571611e1367dd
clementw168 <clementwang2001@gmail.com>
2024-04-05 01:13:10 +0200

    remove small batch size

--- src/classifier.py
+++ src/classifier.py
@@ -15,7 +15,7 @@ from dataset import AspectDataset
 
 @dataclass
 class Config:
-    batch_size: int = 16 # 32 de base
+    batch_size: int = 32
     learning_rate: float = 1e-4 # 3e-3
     num_epochs: int = 10
     num_labels: int = 3
